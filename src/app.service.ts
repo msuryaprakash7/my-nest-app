3dabbeeb1db7297206ef5054b1d6192852c94fc6
Surya Prakash <159560511+msuryaprakash7@users.noreply.github.com>
2025-06-12 20:10:24 +0530

    refactor: welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,30 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<html><body><h1>Hello World!</h1><p>Welcome to the EC2 NestJS Application with Docker</p></body></html>';
+    return `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              margin: 0;
+              padding: 20px;
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              font-size: 18px;
+              color: #666;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+          <p>Welcome to the EC2 NestJS Application with Docker</p>
+        </body>
+      </html>
+    `;
   }
 }
